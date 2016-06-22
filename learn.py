fd86d0a632bcd81752599c2605485306f69d4923
Kyle Laplante <klaplante@twitter.com>
2016-06-22 16:39:42 -0600

    oops

--- learn.py
+++ learn.py
@@ -5,7 +5,7 @@ import sopel
 
 rcache = redis.Redis(host="127.0.0.1", port="6379")
 
-@sopel.module.commands("learn2")
+@sopel.module.commands("learn")
 def learn(bot, trigger):
   tmp = trigger.group(2)
   tmp = tmp.split()
@@ -63,7 +63,7 @@ def learn(bot, trigger):
   else:
     bot.say("Use .learn [add|del] key <value>")
 
-@sopel.module.commands("\.\.")
+@sopel.module.commands("\.")
 def get(bot, trigger):
   key = trigger.group(2)
   if len(key.split()) > 1:
